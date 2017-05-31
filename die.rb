1655c75c8ce7a431caff87713ce4fd92bb8d08aa
vixplows <vicky@vickyplows.co.uk>
2017-05-31 14:11:29 +0100

    wrote dice roll method

--- die.rb
+++ die.rb
@@ -2,6 +2,16 @@ class Die
 
   attr_accessor :sides
 
-  def initialize()
-    @sides = []
+  def initialize(sides)
+    @sides = sides
   end
+
+  def number_of_sides(dice)
+    return @sides.count()
+  end
+
+  def roll()
+    return @sides.sample
+  end
+
+end
