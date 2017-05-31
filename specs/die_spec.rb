d82cfa5a12eb48d7a5bfe18164ff7cc6bc2a7889
vixplows <vicky@vickyplows.co.uk>
2017-05-31 14:03:46 +0100

    Pass number_of_sides_test

--- specs/die_spec.rb
+++ specs/die_spec.rb
@@ -3,9 +3,15 @@ require('minitest/rg')
 require_relative('../player.rb')
 require_relative('../board.rb')
 require_relative('../game.rb')
+require_relative('../die.rb')
 
-class DieTest < Minitest::Test
+class TestDie < MiniTest::Test
 
   def setup
     @dice = Die.new([1, 2, 3, 4, 5, 6])
   end
+
+  def test_number_of_sides
+    assert_equal(6, @dice.sides.length())
+  end
+end
